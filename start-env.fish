cc6c24a399156eb02b378b00a1a380d7ec5d8a24
interestingLSY <interestingLSY@gmail.com>
2024-04-22 13:32:20 +0800

    Add a script to launch the env on Shanghai AI Lab

---
+++ start-env.fish
@@ -0,0 +1,3 @@
+conda activate deepspeed
+export MAX_JOBS=32
+export LD_LIBRARY_PATH=/mnt/petrelfs/share/gcc/mpfr-4.1.0/lib/:/mnt/petrelfs/share/cuda-11.8/lib64/stubs/:/mnt/petrelfs/share/cuda-11.8/lib64
